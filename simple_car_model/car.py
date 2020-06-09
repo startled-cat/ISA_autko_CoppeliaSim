87e9c82f09679b0d2df510841ba202e2b85d080b
Bartosz Świstacz <bartoszswistacz@gmail.com>
2020-06-09 23:47:23 +0200

    small adjustments

--- simple_car_model/car.py
+++ simple_car_model/car.py
@@ -28,8 +28,8 @@ class Car:
         self.turning_time = 1.80
 
         self.rotation_force = 100
-        #self.map = Map(13, 13, [12, 8])#old labirynth01
-        self.map = Map(40, 40, [20, 20])#new labirynth02
+        self.map = Map(13, 13, [12, 8])#old labirynth01
+        #self.map = Map(40, 40, [20, 20])#new labirynth02
         #self.clientID = 0
 
         self.sensors = sensors
